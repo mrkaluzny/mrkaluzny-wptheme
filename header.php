76890f2e915afc4e935d88253251b32a3d1b2d42
mrkaluzny <wojciech.kaluny@me.com>
2016-06-14 15:04:10 +0200

    Test commit

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The header for our theme.
- *
+ * Testing commits.
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
