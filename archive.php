12427971600011d5366d89c388f37bb62a2f88d6
Wojciech Kaluzny <wojciech.kaluzny@me.com>
2016-06-23 22:58:39 +0200

    Archive page completed.

--- archive.php
+++ archive.php
@@ -9,19 +9,22 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+<div class="wrapper"></div>
+	<div class="container">
+		<div class="row">
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<div class="col-xs-12">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</div><!-- .page-header -->
+		</div>
 
+			<div class="row">
+				<div class="col-md-9 col-sm-12">
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
@@ -42,10 +45,12 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
-		</main><!-- #main -->
+	</div>
+	<div class="col-md-3 hidden-xs hidden-sm">
+		<?php get_sidebar(); ?>
+	</div>
+	</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
-get_footer();
+get_footer('default');
