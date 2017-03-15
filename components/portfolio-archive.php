dd2a2cbd1f13c3da19f6c22ebcb811eee55a3d51
Wojciech Kaluzny <wojciech.kaluzny@me.com>
2017-03-15 18:52:19 +0100

    Contact page added

---
+++ components/portfolio-archive.php
@@ -0,0 +1,34 @@
+<section class="portfolio">
+  <div class="container">
+    <div class="row">
+      <div class="portfolio-grid">
+
+        <?php
+        $args = array(
+          'post_type' => 'portfolio',
+          'posts_per_page' => 6,
+        );
+        $opinie = new WP_Query( $args );
+        if( $opinie ):
+          while( $opinie->have_posts() ):
+            $opinie->the_post();
+        ?>
+
+        <a href="<?php the_permalink(); ?>">
+          <div class="portfolio-item">
+            <img src="<?php echo the_post_thumbnail_url('large'); ?>" alt="">
+            <div class="description">
+              <div class="content">
+                <h2><?php the_title();?></h2>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <?php endwhile; endif;?>
+        <?php wp_reset_query(); ?>
+
+      </div>
+    </div>
+  </div>
+</section>
--- page-tempalates/contact-page.php
+++ page-tempalates/contact-page.php
@@ -6,30 +6,65 @@
 
 get_header(); ?>
 
-<div class="wrapper"></div>
-<section>
-  <div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-      <h2 class="text-center mar-side-50"><?php the_field('short') ?></h2>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <?php if (have_posts()) : while (have_posts()) : the_post();?>
-        <?php the_content(); ?>
-      <?php endwhile; endif; ?>
-    </div>
-  </div>
-</div>
-</section>
+<section class="hero __small">
+        <div class="container flex">
+          <div class="row text-center">
+            <h1 style="text-align:center;">Porozmawiajmy</h1>
+            <h2>O twoim projekcie</h2>
+          </div>
+        </div>
+      </section>
 
-<div class="wrapper"></div>
+      <section class="contact">
+        <div class="container">
+          <div class="row">
 
-<!-- GOOGLE MAP -->
-		<div id="module-maps">
-			<div id="map"></div>
-		</div>
-<!-- /GOOGLE MAP -->
+              <div class="card">
+                <div class="row">
+                  <div class="col-md-4 photo" style="background-image:url('assets/img/poznan.jpg');">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="contact-info">
+                      <h5>Our office</h5>
+                      <h2>Headquaters</h2>
+                      <p>Plac Europejski 2 <br /> 00-844 Warsaw, Poland</p>
+                      <hr>
+                      <p>87 887 88 23</p>
+                      <p><a href="#">wk@mrkaluzny.pl</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section class="contact-component __underlay">
+        <div class="container">
+          <div class="row">
+            <h2 class="text-center component-title">Zapraszam do kontaktu</h2>
+            <div class="col-md-12">
+              <form action="/" class="form">
+                <div class="form-group">
+                  <label>Imię i nazwisko</label>
+                  <input class="input" />
+                </div>
+                <div class="form-group">
+                  <label>Adres email *</label>
+                  <input type="email" class="input" />
+                </div>
+                <div class="form-group">
+                  <label>W czym mogę pomóc?</label>
+                  <textarea class="input"></textarea>
+                </div>
+                <div class="form-group text-center">
+                  <input type="submit" class="btn-basic __contact-form" value="Wyślij"/>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
 
 <?php get_footer(); ?>
