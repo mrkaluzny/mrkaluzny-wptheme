ffbb7c6b44303befdbacb66b811e455b3793e039
Wojciech Kaluzny <wojciech.kaluzny@me.com>
2017-03-17 00:13:40 +0100

    Changes to styles

---
+++ single-services.php
@@ -0,0 +1,196 @@
+<?php get_header(); ?>
+
+<section class="hero __small" style="background-image: url('https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/CWYK8CLC61.jpg')">
+        <div class="container flex">
+          <div class="row text-center">
+            <h1 style="text-align:center;">Strony internetowe</h1>
+            <h2></h2>
+          </div>
+        </div>
+      </section>
+
+      <section class="oferta">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 information">
+              <h1 class="subtitle">Nowoczesne strony internetowe</h1>
+              <p class="content">Ruby is a modern programming language that allows developers to build all kinds of applications, from small scripts to enterprise web platforms. Ruby on Rails is a framework – set of tools built on top of Ruby aimed at quickly developing web applications. Some of the benefits of this technology are:</p>
+              <ul>
+                <li>Emphasis on productivity</li>
+                <li>Large and growing community of developers</li>
+                <li>Huge ecosystem so the programmer doesn’t need to reinvent the wheel</li>
+              </ul>
+
+            </div>
+
+            <div class="col-md-6">
+              <img class="image" src="assets/img/webdevelopment.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="oferta">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <img class="image" src="assets/img/webdevelopment.svg" alt="">
+            </div>
+            <div class="col-md-6 information">
+              <h1 class="subtitle">Rozwiązania dopasowane do twoich potrzeb</h1>
+              <p class="content">We will talk to you about your product vision and decide with you if we are a good fit for your project. If we are, we’ll send you a project estimation for your review. Then we’ll pick the right Ruby on Rails development team for you and start your project using Scrum – an Agile development process.</p>
+              <ul>
+                <li>Emphasis on productivity</li>
+                <li>Large and growing community of developers</li>
+                <li>Huge ecosystem so the programmer doesn’t need to reinvent the wheel</li>
+              </ul>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section class="oferta __gray">
+        <div class="container">
+          <div class="row text-center">
+            <h1 class="subtitle">Korzystam z następujących technologii</h1>
+            <ul class="technology-used">
+              <li><img class="img-responsive" src="https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png" /></li>
+              <li><img class="img-responsive" src="https://ohdoylerules.com/content/images/css3.svg" /></li>
+              <li><img class="img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Boostrap_logo.svg/2000px-Boostrap_logo.svg.png" /></li>
+              <li><img class="img-responsive" src="https://s.w.org/about/images/logos/wordpress-logo-notext-rgb.png" /></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="oferta">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h1 class="subtitle">Co mogę dla Ciebie zrobić</h1>
+              <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptas voluptates, a enim vel, aliquam in dolorum voluptatibus reiciendis, unde odio eveniet adipisci, numquam itaque rem aperiam? Reiciendis esse, minima!</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <h2 class="subtitle">Landing Page</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">WordPress</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">Rozwiązania Ecommerce</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="portfolio __oferta">
+        <div class="container">
+          <div class="row title">
+            <div class="col-md-12">
+              <h1 class="subtitle">Wybrane realizacje</h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="portfolio-grid">
+
+              <div class="portfolio-item">
+                <img src="http://mrkaluzny.com/wp-content/uploads/2016/07/zmien-zdanie-mrkaluzny.jpg" alt="Strona Internetowa Zmień Zdanie">
+                <div class="description">
+                  <div class="content">
+                    <h2>Zmień Zdanie Website</h2>
+                    <a href="#" class="btn-basic __white">Sprawdź</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="portfolio-item">
+                <img src="assets/portfolio/casa-morretti.jpg" alt="Strona Internetowa Zmień Zdanie">
+                <div class="description">
+                  <div class="content">
+                    <h2>Zmień Zdanie Website</h2>
+                    <a href="#" class="btn-basic __white">Sprawdź</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="portfolio-item">
+                <img src="assets/portfolio/wirtualne_biuro.jpg" alt="Strona Internetowa Zmień Zdanie">
+                <div class="description">
+                  <div class="content">
+                    <h2>Zmień Zdanie Website</h2>
+                    <a href="#" class="btn-basic __white">Sprawdź</a>
+                  </div>
+                </div>
+              </div>
+              <div class="portfolio-item">
+                <img src="assets/portfolio/wirtualne_biuro.jpg" alt="Strona Internetowa Zmień Zdanie">
+                <div class="description">
+                  <div class="content">
+                    <h2>Zmień Zdanie Website</h2>
+                    <a href="#" class="btn-basic __white">Sprawdź</a>
+                  </div>
+                </div>
+              </div>
+              <div class="portfolio-item">
+                <img src="assets/portfolio/wirtualne_biuro.jpg" alt="Strona Internetowa Zmień Zdanie">
+                <div class="description">
+                  <div class="content">
+                    <h2>Zmień Zdanie Website</h2>
+                    <a href="#" class="btn-basic __white">Sprawdź</a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="oferta">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h1 class="subtitle">Dlaczego warto?</h1>
+              <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptas voluptates, a enim vel, aliquam in dolorum voluptatibus reiciendis, unde odio eveniet adipisci, numquam itaque rem aperiam? Reiciendis esse, minima!</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <h2 class="subtitle">Przejrzysta umowa</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">Jakość</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">Pasja</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <h2 class="subtitle">Przejrzysta umowa</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">Jakość</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+            <div class="col-md-4">
+              <h2 class="subtitle">Pasja</h2>
+              <p class="content">Lorem ipsum dolor sit amet, cons adipisicing elit. A modi hic eveniet vero repellat ab aperiam dicta excepturi esse similique, eius nemo eaque doloremque voluptates sit repellendus provident nisi praesentium.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+<?php get_footer(); ?>
