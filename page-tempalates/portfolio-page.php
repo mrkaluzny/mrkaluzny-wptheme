5df8526a5e1dffcd60df6d35b6b9ef8d876b5e37
mrkaluzny <wojciech.kaluzny@me.com>
2017-06-16 16:44:26 +0200

    Portfolio page updated

--- page-tempalates/portfolio-page.php
+++ page-tempalates/portfolio-page.php
@@ -17,14 +17,23 @@ get_header(); ?>
 <section class="portfolio-single __gray">
   <div class="container">
     <div class="row">
-      <div class="col-md-12 text-center">
+      <div class="col-md-10 col-md-offset-1 text-center content">
         <?php the_content(); ?>
       </div>
     </div>
   </div>
 </section>
 
-
-<?php get_template_part('components/portfolio', 'archive'); ?>
+<section class="portfolio-section portfolio-section--dark">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="portfolio-list">
+          <?php get_template_part('components/portfolio'); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
