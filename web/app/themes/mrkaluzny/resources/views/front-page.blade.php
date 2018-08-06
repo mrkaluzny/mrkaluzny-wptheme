643e2a537e394b043d4ace26ad154dc7f6816719
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-06 20:27:30 +0200

    Hero added to frontpage

---
+++ web/app/themes/mrkaluzny/resources/views/front-page.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+  <section class="hero background--down">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="title title--hero">Get from idea to a bussiness.</h1>
+          <h2 class="title title--sub">You might be searching through job boards to find someone for your project. Why waste your time?  Work with tested, dedicated developer!</h2>
+          <button class="btn btn--rounded btn--outline btn--green btn--large btn--center">Get Started</button>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
