de66418cf90468b8d87b936bd521a80301f65804
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-07 22:14:49 +0200

    Navbar scroll up navigation enabled

--- web/app/themes/mrkaluzny/resources/assets/scripts/components/Navbar.jsx
+++ web/app/themes/mrkaluzny/resources/assets/scripts/components/Navbar.jsx
@@ -13,11 +13,15 @@ export default class Navbar extends React.Component {
       status: '',
       content: '',
       isMenuOpen: false,
+      currentScroll: 0,
+      isNavigationVisible: false,
     }
     this.activateMenu = this.activateMenu.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
   componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
     axios.get(Helper.getAPIRouteForResource('settings'))
     .then(res => {
       this.setState({
@@ -27,22 +31,49 @@ export default class Navbar extends React.Component {
     })
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
   activateMenu() {
     this.setState(prevState => ({
       isMenuOpen: !prevState.isMenuOpen,
     }))
   }
 
+  handleScroll () {
+    const lastScrollY = this.state.currentScroll;
+    const currentScrollY = window.scrollY;
+    console.log(`Current scroll: ${currentScrollY} Last Scroll: ${lastScrollY}`)
+
+    if (currentScrollY > lastScrollY) {
+      this.setState({ isNavigationVisible: false });
+    } else if (currentScrollY < lastScrollY) {
+      this.setState({ isNavigationVisible: true });
+    }
+    this.setState({ currentScroll: currentScrollY });
+  }
+
 
   render() {
     let homeURL = window.location.href
+    let navigationClass
+    if (this.state.currentScroll < 66 && !this.state.isNavigationVisible || this.state.currentScroll < 3) {
+      navigationClass = 'navigation'
+    } else {
+      navigationClass = 'navigation navigation--fixed navigation--animate' + (this.state.isNavigationVisible ? ' navigation--visible' : ' navigation--hidden');
+    }
+
+    if (this.state.isMenuOpen) {
+      navigationClass += ' navigation--open'
+    }
     let btnClass = 'btn btn--nav' + (this.state.isMenuOpen ? ' btn--active' : '')
     let brandClass = 'brand' + (this.state.isMenuOpen ? ' brand--white' : '')
     let navClass = 'mobile-nav' + (this.state.isMenuOpen ? ' mobile-nav--active' : '')
 
     return (
       <header className="header">
-        <nav className="navigation">
+        <nav className={navigationClass}>
           <div className="container">
             <div className="navigation__inner">
               <a className={brandClass} href={homeURL}>mrkaluzny</a>
