e04919e0e14dce3e8b6a4f574ef8d0811880ab51
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-08 14:38:58 +0200

    Testimonials component updated

---
+++ web/app/themes/mrkaluzny/resources/assets/scripts/components/Testimonials.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {Helper} from '../Helper.js';
+import axios from 'axios';
+
+export default class Testimonials extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
+    this.state = {
+      testimonials: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    axios.get(Helper.getAPIRouteForResource('testimonials'))
+    .then(res => {
+      this.setState({
+        testimonials: res.data,
+      })
+    })
+  }
+
+
+  render() {
+
+    const cards = this.state.testimonials.map( (item) => {
+      return (
+        <article className="card card--testimonial" key={item.id}>
+          <img src={item.image.small} alt="" className="card__image card__image--testimonial"/>
+          <h1 className="card__name">{item.name}</h1>
+          <h2 className="card__position">{item.position} at {item.company}</h2>
+          <div className="card__content">{item.content}</div>
+        </article>
+      )
+    })
+
+    return (
+      <section className="section section--testimonials background--up">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="title">
+                <h2 className="title__top">Testimonials</h2>
+                <h1 className="title__main">Some of my happy clients</h1>
+              </div>
+              <div className="testimonials__feed">
+                { cards }
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
--- web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
+++ web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Availability from '../components/Availability.jsx';
 import Navbar from '../components/Navbar.jsx';
+import Testimonials from '../components/Testimonials.jsx';
 
 export default {
   init() {
     ReactDOM.render(<Availability />, document.getElementById('availability'))
     ReactDOM.render(<Navbar />, document.getElementById('navbar'))
+    ReactDOM.render(<Testimonials />, document.getElementById('testimonials'))
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
