432e51e9bda85b97fe68b17d93418284df0928e5
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-08 14:39:10 +0200

    Testimonials styled

--- web/app/themes/mrkaluzny/resources/views/front-page.blade.php
+++ web/app/themes/mrkaluzny/resources/views/front-page.blade.php
@@ -6,10 +6,11 @@
       <div class="row">
         <div class="col-12">
           <h1 class="title title--hero">Get from idea to a bussiness.</h1>
-          <h2 class="title title--sub">You might be searching through job boards to find someone for your project. Why waste your time?  Work with tested, dedicated developer!</h2>
+          <h2 class="title title--sub">You might be searching through job boards to find someone for your project. Why waste your time? Work with tested, dedicated developer!</h2>
           <button class="btn btn--rounded btn--outline btn--green btn--large btn--center">Get Started</button>
         </div>
       </div>
     </div>
   </section>
+  <section id="testimonials"></section>
 @endsection
