26b388aba6d28301580d322b9fc4ad01538e9824
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-30 13:32:10 +0200

    RecentArticles added to common components

--- web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
+++ web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
@@ -4,6 +4,7 @@ import Availability from '../components/Availability.jsx';
 import Navbar from '../components/Navbar.jsx';
 import Testimonials from '../components/Testimonials.jsx';
 import RecentProjects from '../components/RecentProjects.jsx';
+import RecentArticles from '../components/RecentArticles.jsx';
 
 export default {
   init() {
@@ -11,6 +12,7 @@ export default {
     ReactDOM.render(<Navbar />, document.getElementById('navbar'))
     ReactDOM.render(<Testimonials />, document.getElementById('testimonials'))
     ReactDOM.render(<RecentProjects />, document.getElementById('recent-projects'))
+    ReactDOM.render(<RecentArticles />, document.getElementById('recent-articles'))
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
