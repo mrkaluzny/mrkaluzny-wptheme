76db535b270eaf3626ed4960eef99464184ece5a
mrkaluzny <wojciech.kaluzny@me.com>
2018-08-30 13:31:51 +0200

    Helper class -> methods added to retrieve viewport dimensions

--- web/app/themes/mrkaluzny/resources/assets/scripts/Helper.js
+++ web/app/themes/mrkaluzny/resources/assets/scripts/Helper.js
@@ -4,4 +4,24 @@ export const Helper = {
     (version ? (APIver = version) : (APIver = 'v1'))
     return window.location.href + '/wp-json/api/' + APIver + '/' + resource_name
   },
+
+  getInnerWidth() {
+    let w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth;
+
+    return x
+  },
+
+  getInnerHeight() {
+    let w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+    return y
+  },
 }
