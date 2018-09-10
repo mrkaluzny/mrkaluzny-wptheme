d1bda790001438ccef75afd695674e57b3c49fbf
mrkaluzny <wojciech.kaluzny@me.com>
2018-09-10 17:27:29 +0200

    Services items updated to use css properties instead of tilt.js

--- web/app/themes/mrkaluzny/resources/assets/scripts/routes/home.js
+++ web/app/themes/mrkaluzny/resources/assets/scripts/routes/home.js
@@ -1,15 +1,8 @@
 import 'jquery';
-import 'tilt.js';
 
 export default {
   init() {
     // JavaScript to be fired on the home page
-    $('.service').tilt({
-      maxTilt: 10,
-      perspective: 1500,
-      scale: 1.1,
-      speed: 400,
-    })
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
