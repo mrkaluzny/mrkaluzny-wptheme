dd9fa8189a3283c07b28c94fb7637a774b759305
mrkaluzny <wojciech.kaluzny@me.com>
2018-10-11 21:07:41 +0200

    Scripts: added a filter for rendering React components

--- web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
+++ web/app/themes/mrkaluzny/resources/assets/scripts/routes/common.jsx
@@ -1,3 +1,4 @@
+import 'jquery';
 import AOS from 'aos';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -10,13 +11,21 @@ import RecentArticles from '../React/RecentArticles.jsx';
 export default {
   init() {
     AOS.init();
-    ReactDOM.render(<Availability />, document.getElementById('availability'))
-    ReactDOM.render(<Navbar />, document.getElementById('navbar'))
-    ReactDOM.render(<Testimonials />, document.getElementById('testimonials'))
-    ReactDOM.render(<RecentProjects />, document.getElementById('recent-projects'))
-    ReactDOM.render(<RecentArticles />, document.getElementById('recent-articles'))
+    checkIfElementExistsAndRenderReactComponent(<Availability />, 'availability')
+    checkIfElementExistsAndRenderReactComponent(<Navbar />, 'navbar')
+    checkIfElementExistsAndRenderReactComponent(<Testimonials />, 'testimonials')
+    checkIfElementExistsAndRenderReactComponent(<Testimonials />, 'testimonials')
+    checkIfElementExistsAndRenderReactComponent(<RecentProjects />, 'recent-projects')
+    checkIfElementExistsAndRenderReactComponent(<RecentArticles />, 'recent-articles')
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
   },
 };
+
+
+function checkIfElementExistsAndRenderReactComponent(Component, elementID) {
+  if ($(`#${elementID}`).length > 0) {
+    ReactDOM.render(Component, document.getElementById(elementID))
+  }
+}
