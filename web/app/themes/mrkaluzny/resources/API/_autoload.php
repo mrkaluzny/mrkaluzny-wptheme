20b08f817038691255b4a51234c9ed2d177c6159
mrkaluzny <wojciech.kaluzny@me.com>
2018-10-24 16:02:12 +0200

    Article hero overlay updated

--- web/app/themes/mrkaluzny/resources/API/_autoload.php
+++ web/app/themes/mrkaluzny/resources/API/_autoload.php
@@ -1,8 +1,8 @@
 <?php
 // Endpoints for API
+require_once('navigation.php');
 require_once('settings.php');
 require_once('testimonials.php');
 require_once('projects.php');
 require_once('posts.php');
-require_once('navigation.php');
 ?>
